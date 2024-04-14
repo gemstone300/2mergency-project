40a958c2d59cff3dafda6455df78c3a6e11bd243
gemstone300 <claire.gemstone300@gmail.com>
2024-04-14 23:12:12 +0900

    test cicd

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ pm2 delete 2mergency-project || true
 # 앱을 PM2로 다시 등록 및 시작
 pm2 start npm --name "2mergency-project" -- run start
 
-# PM2 설정 저장 (서버 재시작시 앱 자동 시작을 위함)
+# PM2 설정 저장 (서버 재시작시 앱 자동 시작을 위함) ###
 pm2 save
